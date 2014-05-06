647b45346173852b95abd7f43dfe416f242523fa
piwik-la <admin@piwik.la>
2014-05-06 23:45:10 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#https://github.com/piwikla/debian7-nginx
+
+#Add Piwik package repository
+echo "deb http://debian.piwik.org/ piwik main" >> /etc/apt/sources.list.d/piwik.list
+echo "deb-src http://debian.piwik.org/ piwik main" >> /etc/apt/sources.list.d/piwik.list
+wget -qO - http://debian.piwik.org/repository.gpg | sudo apt-key add -
+
+#Update
+sudo apt-get update
+
+#Install Nginx
+sudo apt-get install nginx
+
+#Install Piwik
+sudo apt-get install piwik
+
+#Install MySQL
+sudo apt-get install mysql-server
+sudo mysql_install_db
+sudo mysql_secure_installation
+
+#Install phpMyAdmin
+apt-get install phpmyadmin
+sudo ln -s /usr/share/phpmyadmin/ /usr/share/piwik/dba
+
+#Install PHP 
+sudo apt-get install php5-fpm php5-mysql
+
+#Install PECL
+sudo apt-get install php5-geoip php5-dev libgeoip-dev
+sudo pecl install geoip
+
+#Restart Service
+sudo service php5-fpm restart
+sudo service nginx restart
+sudo service mysql restart
